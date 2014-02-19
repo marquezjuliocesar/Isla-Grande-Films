d62720b0463453f912e43dde2adb01ca28eb5455
marquezjuliocesar <juliocesarmarquez88@gmail.com>
2014-02-19 20:50:36 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html> <!-- Define el tipo de doc -->
+<html lang="en"> <!-- Inicia documento HTML con idioma EN -->
+<head> <!-- Cabecera del documento -->
+	<meta charset="UTF-8"> <!-- Metadatos: Define que el charset es UTF-8 -->
+	<title>Document</title>
+	<link rel="stylesheet" href="estilo.css"> <!-- Titulo -->
+</head>
+<body> <!-- Inicia el cuerpo de la página -->
+
+	<section id="contenedor">
+		
+		<nav>
+			<ul id="menu">
+				<li><a href="#">Inicio</a></li>
+				<li><a href="#">Quienes Somos</a></li>
+				<li><a href="#">Blog</a></li>
+				<li><a href="#">Contacto</a></li>
+			</ul>
+		</nav>
+
+		<header>
+			<h1>Isla Grande Films</h1>
+			<p>Isla Grande Films es un proyecto de Productora Audiovisual Independiente, creada en la Patagonia Argentina.</p>
+		</header>
+
+		<footer>
+			<span><a href="#">Sitio realizado por Julio Marquez</a></span>
+		</footer>
+	</section>
+	
+
+</body>
+</html>
